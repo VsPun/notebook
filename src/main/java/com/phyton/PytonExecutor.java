e7ee33cfc869a1ef12478ceea9410afc4577ccb0
Laila1985 <laila.bouhadou@gmail.com>
2019-03-28 01:39:27 +0300

    Python exector clean code

--- src/main/java/com/phyton/PytonExecutor.java
+++ src/main/java/com/phyton/PytonExecutor.java
@@ -58,21 +58,4 @@ public class PytonExecutor {
 
 	}
 
-	/**
-	 * 
-	 * Pour tester rapidement utiliser main this example works for me with:
-	 * -Dpython.console.encoding=UTF-8 in VM arguments
-	 */
-	public static void main(String[] args) throws PyException {
-		PythonInterpreter interp = new PythonInterpreter();
-		System.out.println("Hello, world from Java");
-		interp.set("a", new PyInteger(42));
-		interp.exec("print a");
-		interp.exec("x = 2+2");
-		PyObject x = interp.get("x");
-		System.out.println("x: " + x);
-		System.out.println("Goodbye ");
-		interp.close();
-	}
-
 }
