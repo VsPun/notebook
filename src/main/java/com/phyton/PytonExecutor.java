1aa921933d199acaa7a89c661857ce8f8d2a5b37
Laila1985 <laila.bouhadou@gmail.com>
2019-03-28 00:33:39 +0300

    Python executor and unitTest

--- src/main/java/com/phyton/PytonExecutor.java
+++ src/main/java/com/phyton/PytonExecutor.java
@@ -1,9 +1,12 @@
 package com.phyton;
 
 import java.io.IOException;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.python.util.PythonInterpreter;
+import org.springframework.util.StringUtils;
 
 import ch.qos.logback.classic.Logger;
 
@@ -12,29 +15,64 @@ import org.python.core.*;
 public class PytonExecutor {
 
 	private Logger log;
+	public String variable;
 
 	public String execute(String code) throws IOException {
 
-		PythonInterpreter interpreter = null;
-
+		PyObject x = null;
 		try {
-			Properties p = new Properties();
-			p.setProperty("python.path", "C:\\Users\\laila\\.m2\\repository\\org\\python\\jython\\2.7.0");
-			p.setProperty("python.home", "C:\\Users\\laila\\.m2\\repository\\org\\python\\jython\\2.7.0");
-			p.setProperty("python.prefix", "C:\\Users\\laila\\.m2\\repository\\org\\python\\jython\\2.7.0");
-			PythonInterpreter.initialize(System.getProperties(), p, new String[] {});
-			interpreter = new PythonInterpreter();
+			PythonInterpreter interp = new PythonInterpreter();
+			System.out.println("****** PYthon code process Started *******");
+			if (code.contains("print")) {
+				// remove print
+				String executedCode = StringUtils.replace(code, "print", " ");
+				if (variable == null) {
+					interp.set("a", new PyInteger(0));
+					interp.exec("a= " + executedCode);
+					x = interp.get("a");
+				} else {
+					interp.set("a", new PyInteger(Integer.parseInt(variable)));
+					interp.exec("print a ");
+					interp.exec("a=" + executedCode);
+					x = interp.get("a");
 
-			interpreter.exec(code);
-			interpreter.close();
-			System.out.println("val : " + code);
-			return "2";
+				}
+
+				System.out.println("****** PYthon code ended  *****************");
+				interp.close();
+			} else if (code.contains("=")) {
+				setVariable(code);
+				return "";
+			}
 
 		} catch (Exception ex) {
 			log.error("Exception while creating python interpreter: " + ex.toString());
 		}
 
-		return "2"; 
+		return x.toString();
+	}
+
+	public void setVariable(String var) {
+
+		this.variable = var;
+
+	}
+
+	/**
+	 * 
+	 * Pour tester rapidement utiliser main this example works for me with:
+	 * -Dpython.console.encoding=UTF-8 in VM arguments
+	 */
+	public static void main(String[] args) throws PyException {
+		PythonInterpreter interp = new PythonInterpreter();
+		System.out.println("Hello, world from Java");
+		interp.set("a", new PyInteger(42));
+		interp.exec("print a");
+		interp.exec("x = 2+2");
+		PyObject x = interp.get("x");
+		System.out.println("x: " + x);
+		System.out.println("Goodbye ");
+		interp.close();
 	}
 
 }
--- src/main/java/com/phyton/controller/NoteBookServerController.java
+++ src/main/java/com/phyton/controller/NoteBookServerController.java
@@ -1,12 +1,13 @@
 package com.phyton.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.stream.Stream;
 
-@Controller
+@RestController
 public class NoteBookServerController {
 
 	/**
@@ -19,7 +20,6 @@ public class NoteBookServerController {
 	@GetMapping("/execute")
 	public String executing(@RequestBody Stream<String> text,
 			Model model) {
-		//model.addAttribute("name", text);
 
 		/*
 		 text
